6ad3f352cb847f4348b4490b4893138668237006
Deepu Mohan Puthrote <git@deepumohan.com>
2022-10-24 12:31:45 +0100

    Remove repressed examples

--- src/cljs_2048/events.cljs
+++ src/cljs_2048/events.cljs
@@ -12,11 +12,6 @@
   db/default-db))
 
 (re-frame/reg-event-db
- ::set-re-pressed-example
- (fn [db [_ value]]
-   (assoc db :re-pressed-example value)))
-
-(re-frame/reg-event-db
  ::start-game
  (fn-traced
   [db [_ _]]
--- src/cljs_2048/subs.cljs
+++ src/cljs_2048/subs.cljs
@@ -8,21 +8,16 @@
    (:name db)))
 
 (re-frame/reg-sub
- ::re-pressed-example
+ ::board
  (fn [db _]
-   (:re-pressed-example db)))
+   (:board db)))
 
 (re-frame/reg-sub
-  ::board
-  (fn [db _]
-    (:board db)))
+ ::score
+ (fn [db _]
+   (:score db)))
 
 (re-frame/reg-sub
-  ::score
-  (fn [db _]
-    (:score db)))
-
-(re-frame/reg-sub
-  ::high-score
-  (fn [db _]
-    (:high-score db)))
+ ::high-score
+ (fn [db _]
+   (:high-score db)))
