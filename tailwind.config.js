862a74650e8062697332e855391d4f2451795d43
Deepu Mohan Puthrote <git@deepumohan.com>
2022-11-06 23:35:32 +0000

    feat: Setup tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: 'jit',
+  content: [
+    './resources/**/*.html',
+    './resources/**/*.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
